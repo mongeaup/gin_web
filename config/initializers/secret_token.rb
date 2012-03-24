498f5ca7b5b696573bfdac4afdafffc88b03fc73
Patrice Mongeau <mongeaup@gmail.com>
2012-03-23 22:20:15 -0400

    Commit initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GinWeb::Application.config.secret_token = '009c4d30ebdab12059e40d18d83e3007d83c759b70e7a546fe5868a539975c27ab3bf75e5e3d4b0a165cda3eef0812fa6190f3eac2578af0a5b2d94a9ee0ce63'
